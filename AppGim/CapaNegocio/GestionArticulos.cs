5b953122f4025f139cdbcee111b0aa196a20873f
unknown <nicolasfediuk@gmail.com>
2022-01-24 00:15:07 -0300

    Empezando capa Gestion

---
+++ AppGim/CapaNegocio/GestionArticulos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Entidades;
+using CapaDao;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class GestionArticulos
+    {
+        DaoProductos dao = new DaoProductos();
+
+        public DataTable getArticulos()
+        {
+            return dao.getTablaProductos();
+        }
+
+        public DataTable getProductosPorId(Productos p)
+        {
+            return dao.getTablaCodProducto(p);
+        }
+
+        public bool ExisteProducto(Productos p)
+        {
+            if (dao.ExisteProducto(p))
+            {
+                return true; 
+            }
+            else
+            {
+                return false;   
+            }
+        }
+    }
+}
---
+++ AppGim/CapaNegocio/GestionCategoriaArticulos.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+using CapaDao;
+
+namespace CapaNegocio
+{
+    public class GestionCategoriaArticulos
+    {
+        DaoCategoriaProducto dao = new DaoCategoriaProducto();
+
+        public DataTable getCategoriasArticulos()
+        {
+            return dao.getTablaCategoria();
+        }
+
+        public DataTable getCategoriaPorId(CategoriasProductos cp)
+        {
+            return dao.getTablaCategoriaId(cp);
+        }
+
+        public bool ExisteCategoriaProducto(CategoriasProductos cp)
+        {
+            if (dao.ExisteCategoria(cp))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AgregarCategoriaProducto(CategoriasProductos cp)
+        {
+            int cantFila = 0;
+            if (dao.ExisteCategoria(cp) == false)
+            {
+                cantFila = dao.AgregarCategoria(cp);
+            }
+
+            if(cantFila == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ModificarCategoriaProducto(CategoriasProductos cp)
+        {
+            bool eliminado = dao.ModificarCategoria(cp);
+            if(eliminado == false)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool EliminarCategoriaProducto(CategoriasProductos cp)
+        {
+            int eliminado = dao.EliminarCategoria(cp);
+
+            if(eliminado == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
